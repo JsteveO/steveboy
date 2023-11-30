575f27eca99d8d2e53a24bfd3594802731534fd1
JsteveO <146886471+JsteveO@users.noreply.github.com>
2023-11-30 10:53:19 -0500

    Create sett.py

---
+++ sett.py
@@ -0,0 +1,28 @@
+import os
+
+
+sets = None
+
+
+token = 'Steveboy'
+
+whatsapp_token = '#token_Whatsapp'
+
+whatsapp_url = '#whatsapp_url'
+
+stickers = {
+    "poyo_feliz": 984778742532668,
+    "perro_traje": 1009219236749949,
+    "perro_triste": 982264672785815,
+}
+
+
+document_url = "#ruta web"
+
+#Las listas listas de whatsapp solo permiten 10 options
+zona1 = ['BELEN', 'CENTRO', 'SUR', 'BELLO', 'OCCIDENTE', 'ORIENTE', 'NORTE', 'SUROESTE', 'URABA']
+zona2 = ['CALDAS', 'MAGDALENA MEDIO', 'CORDOBA', 'TOLIMA', 'SUCRE']
+
+zonas = ['belen', 'centro', 'sur', 'bello', 'occidente', 'oriente',
+        'norte', 'suroeste', 'uraba', 'caldas', 'magdalena medio',
+         'cordoba', 'tolima', 'sucre']
